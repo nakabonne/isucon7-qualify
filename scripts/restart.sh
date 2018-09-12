d7f361ddcf0588464137cdc180e21639efd94e7e
nakabonne <rodriguez.nak@gmail.com>
2018-09-12 14:25:03 +0900

    add successs message

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
+
 cd /home/isucon/isubata
 git pull
+
 sudo cp etc/nginx.conf /etc/nginx/nginx.conf
+
 cd /home/isucon/isubata/webapp/go
 make
+
 sudo /usr/sbin/nginx -t
 sudo service nginx reload
+
 sudo service isubata.golang restart
+
+echo ""
+echo "$(tput setaf 2)Restart Succeeded!!! ✔︎$(tput sgr0)"
